3c5f8a76d365abc595cb9c008c8a6c7b7ca4add7
AprilWarren99 <april.warren314@gmail.com>
2025-12-01 23:31:00 -0330

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,55 @@
+package org.aoc
+
+import java.io.File
+
+fun turnDial(dial: Set<Int>, currentIndex: Int, direction: Char, increment: Int): Int{
+    var ci = currentIndex
+    var inc = increment
+
+//    Handle if increment > 99
+    if(inc >= 100){
+        inc = increment % 100
+    }
+
+
+    if(direction == 'R'){
+        ci += inc
+        if(ci > 99){
+            ci -= 100
+        }
+    }else{
+        ci -= inc
+        if(ci < 0){
+            ci += 100
+        }
+    }
+//    println("Initial: ${currentIndex}, Moving: $direction $inc, after-op: $ci")
+    return ci
+
+}
+
+
+fun main() {
+    val dial: Set<Int> = (0..99).toSet()
+    var currentDialValue: Int = 50
+    var numbersDialedTo: MutableList<Int> = mutableListOf<Int>()
+
+
+    File("src/main/resources/data.txt").forEachLine {
+//        println("$it: [${it.elementAt(0)}, ${it.slice(1..it.lastIndex)}]")
+
+        currentDialValue = (turnDial(
+            dial,
+            currentDialValue,
+            it.elementAt(0),
+            it.slice(1..it.lastIndex).toInt()))
+        numbersDialedTo.add(currentDialValue)
+    }
+    var zeroCount = 0
+    numbersDialedTo.forEach {
+        if(it == 0){
+            zeroCount++
+        }
+    }
+    println("\nThere are $zeroCount 0's")
+}
