ebb6f6e347a7506b1afeb2319b0736a08b94f237
AprilWarren99 <april.warren314@gmail.com>
2025-12-03 13:32:06 -0330

    Day 2 Part 2 - I think I'm close here but I need to work on the checkRepeat function as it's not updating the index correctly

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 import org.aoc.day1.Part1 as D1P1
 import org.aoc.day1.Part2 as D1P2
-import org.aoc.day2.Part1 as D2P1
+import org.aoc.day2.Solution as Day2
 fun main(){
 //    print(D1P1().run())
 //    print(D1P2().run())
-    D2P1(1)
+    Day2(2, 3)
 }
