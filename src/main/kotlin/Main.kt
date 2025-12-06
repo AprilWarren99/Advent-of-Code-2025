b6c1430ed79b1271735c5ab4e1f12b1c9ec8ef72
FocalPointDevelopment <april@focalpointdevelopment.ca>
2025-12-05 21:15:30 -0330

    Day 4 Part 1 - Completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 import day2.Solution as Day2
 import day3.Solution as Day3
+import day4.Solution as Day4
 fun main(){
 //    print(D1P1().run())
 //    print(D1P2().run())
 //    print(Day2(2, 1).getResults()) //Day 2 part 2 not working, moving on to day 3
 //    print("Day 3 part 1's sum is: ${Day3(1, "live").getResult()}")
-    println(Day3(2, "test").getResult())
+//    println(Day3(2, "test").getResult())
 //      print("Day 3 part 2's sum is: ${Day3(2, "live").getResult()}")
+    print(Day4(1, "live").getResults())
 }
---
+++ src/main/kotlin/day4/Solution.kt
@@ -0,0 +1,58 @@
+package day4
+
+import org.w3c.dom.ranges.Range
+import java.io.File
+import java.math.BigInteger
+
+class Solution(val partNumber: Int, val dataToUse: String) {
+    var data: List<String> = mutableListOf()
+    var numberOfValidIDs = 0
+    init{
+        when(dataToUse){
+            "sample" -> data = File("src/main/kotlin/day4/resources/sampleData.txt").readLines()
+            "live" -> data = File("src/main/kotlin/day4/resources/data.txt").readLines()
+            "test" -> data = File("src/main/kotlin/day4/resources/testData.txt").readLines()
+        }
+
+        when(partNumber){
+            1 -> partOne()
+        }
+    }
+    fun partOne(){
+        var readingRanges = true
+        var ranges: MutableList<List<BigInteger>> = mutableListOf()
+        val ids: MutableList<BigInteger> = mutableListOf()
+        data.forEach {
+            if(it == "") {
+                readingRanges = false
+                return@forEach
+            }
+            if(readingRanges){
+                val startRange = it.split("-")[0].toBigInteger()
+                val endRange = it.split("-")[1].toBigInteger()
+                ranges.add(listOf(startRange, endRange))
+            }else{
+                ids.add(it.toBigInteger())
+            }
+        }
+        ids.forEach { id ->
+            var found = false
+            ranges.forEach { range ->
+                if (range[0] <= id && id <= range[1]) {
+                    found = true
+                    return@forEach
+                }
+            }
+            if (found) {
+                numberOfValidIDs++
+                println("$id found, ($numberOfValidIDs)")
+            }
+        }
+
+        println("There are $numberOfValidIDs validIDs")
+        println("id: ${ids.last()}")
+        println("range: ${ranges.last()}")
+    }
+
+    fun getResults(): Int{return numberOfValidIDs}
+}
